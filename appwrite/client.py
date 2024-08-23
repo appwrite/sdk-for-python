b8a377b60915cd1b8deea066102b1d3dc082e595
root <christyjacob4@gmail.com>
2024-08-23 15:27:10 +0000

    chore: update SDKs for appwrite 1.6.0

--- appwrite/client.py
+++ appwrite/client.py
@@ -13,11 +13,11 @@ class Client:
         self._endpoint = 'https://cloud.appwrite.io/v1'
         self._global_headers = {
             'content-type': '',
-            'user-agent' : 'AppwritePythonSDK/6.0.0-rc.2 (${os.uname().sysname}; ${os.uname().version}; ${os.uname().machine})',
+            'user-agent' : 'AppwritePythonSDK/6.0.0 (${os.uname().sysname}; ${os.uname().version}; ${os.uname().machine})',
             'x-sdk-name': 'Python',
             'x-sdk-platform': 'server',
             'x-sdk-language': 'python',
-            'x-sdk-version': '6.0.0-rc.2',
+            'x-sdk-version': '6.0.0',
             'X-Appwrite-Response-Format' : '1.6.0',
         }
 
